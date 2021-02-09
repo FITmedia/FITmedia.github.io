df550d151614e7cbeef853323dc527cce326e607
jklueck <jklueck@thefitmedia.com>
2021-02-09 10:08:23 -0500

    Added tax tools

---
+++ tools/tax/js/main.js
@@ -0,0 +1,32 @@
+var db = {
+	searchIRS: "site:irs.gov ",
+	searchIntuit: "site:ttlc.intuit.com "
+};
+
+function srch(elem) {
+	var id = elem.id;
+	var text = db[id];
+	var search = text + elem.value;
+	var encode = encodeURI(search);
+	var url = "https://www.google.com/search?q="+encode;
+	window.open(url);
+    //location = url;
+}
+
+function setListeners() {
+    var elems = document.getElementsByTagName("input");
+    for (var ea in elems) {
+        if (elems[ea].id) {
+            elems[ea].addEventListener("keypress",
+                (e) => {
+                    if (e.key === "Enter") {
+                        var elem = document.activeElement;
+                        srch(elem);
+                    }
+                }
+            )
+        }
+    }
+}
+
+setTimeout(setListeners, 3000);
